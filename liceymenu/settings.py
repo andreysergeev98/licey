d962354071874ccb67813f3e54ebfb7a5c125104
Andrey sergeev <sergeevandrey98@yandex.ru>
2021-10-18 15:12:09 +0300

    update

--- liceymenu/settings.py
+++ liceymenu/settings.py
@@ -88,6 +88,13 @@ DATABASES = {
 }
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
